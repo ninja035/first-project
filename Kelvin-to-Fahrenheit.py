32b2fd7c5102c598ad8517ec2a0abe2b61e2b0d3
ninja035 <34950272+ninja035@users.noreply.github.com>
2017-12-29 18:46:57 -0500

    Create Kelvin-to-Fahrenheit.py

---
+++ Kelvin-to-Fahrenheit.py
@@ -0,0 +1,9 @@
+import random as rand
+def k_to_f(n):
+  kelvin == n
+  celsius == kelvin - 273.15
+  fahrenheit == (celsius * 9) /5 + 32
+
+n = rand.randint(1,10000)
+print(n)
+print(round(fahrenheit, 3))
