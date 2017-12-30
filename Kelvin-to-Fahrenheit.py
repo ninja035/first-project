34098240c1f499ad143b7ec133a5647bcd915ff3
ninja035 <34950272+ninja035@users.noreply.github.com>
2017-12-29 19:07:15 -0500

    Update Kelvin-to-Fahrenheit.py

--- Kelvin-to-Fahrenheit.py
+++ Kelvin-to-Fahrenheit.py
@@ -3,7 +3,8 @@ def k_to_f(n):
   kelvin == n
   celsius == kelvin - 273.15
   fahrenheit == (celsius * 9) /5 + 32
+  return fahrenheit
 
 n = rand.randint(1,10000)
 print(n)
-print(round(fahrenheit, 3))
+print(k_to_f(n))
